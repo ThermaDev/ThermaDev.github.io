73485a0ce60545a20f18d89e5c745c6857eb15ec
Thermalight <alumite10@gmail.com>
2024-02-16 09:35:00 +0100

    add metal on scavenge button click

--- src/Components/Resource.tsx
+++ src/Components/Resource.tsx
@@ -1,8 +1,11 @@
+import { useResources } from "../Contexts/ResourceContext";
+
 function Resource() {
+    let updateMetal = useResources().updateMetal;
     return (
         <div className='resource-content'>
             <h2 className='section-title'>Resources</h2>
-            <button>Scavenge</button>
+            <button onClick={() => updateMetal(1)}>Scavenge</button>
         </div>
     )
 }
