6d21772e357f25dc5419c82e0c05f701739980d2
Thermalight <alumite10@gmail.com>
2024-02-16 09:34:38 +0100

    rig variables to display

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,12 +1,16 @@
+import { useResources } from "../Contexts/ResourceContext";
 import Dashboard from "./Dashboard";
 
 function Main() {
+  let money = useResources().money;
+  let metal = useResources().metal;
+  
   return (
     <div className='main-content'>
       <h2 className='section-title'>Dashboard</h2>
       <div className='resources'>
-        <p>$0.00</p> 
-        <p>Metal: 0</p>
+        <p>${money}</p> 
+        <p>Metal: {metal}</p>
       </div>
       <Dashboard />
     </div>
